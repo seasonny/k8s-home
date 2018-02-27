8821a4a0ea7e241cec98c173660b8577590c1d26
Chris <chrise@moo.com>
2018-02-27 21:11:27 +0000

    Floorplan working!

--- home-assistant/copy-www-to-node.sh
+++ home-assistant/copy-www-to-node.sh
@@ -4,4 +4,4 @@ set -e
 export NODE_IP="192.168.0.101"
 
 echo "[ INFO ] Copying files to $NODE_IP" 
-scp -r config/www/* pi@$NODE_IP :/k8s-home/home-assistant/config/www
+scp -r config/www/* pi@$NODE_IP:/k8s-home/home-assistant/config/www
